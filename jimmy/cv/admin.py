7dea0caac05ff2c6f41666f4bb73d76a0c54266a
jimmy0622 <117650590+jimmy0622@users.noreply.github.com>
2024-07-08 13:55:55 +0800

    Delete jimmy directory

--- jimmy/cv/admin.py
+++
@@ -1,9 +0,0 @@
-# cv/admin.py
-from django.contrib import admin
-from .models import Profile, Experience, Education, ProgramSkill, Project
-
-admin.site.register(Profile)
-admin.site.register(Experience)
-admin.site.register(Education)
-admin.site.register(ProgramSkill)
-admin.site.register(Project)
